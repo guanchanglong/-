dc91ba5afe207ec1bbd4b6ec6ef631c4a1f5d1f6
guanchanglong <506921079@qq.com>
2020-01-01 16:27:42 +0800

    1_1

---
+++ 3.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_DEPRECATE 0
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i;
+	for (i = 1; i < 10000; i++)
+	{
+		if (sqrt(float(i + 100)) == floor(sqrt(float(i + 100))+0.5) && sqrt(float(i + 268)) == floor(sqrt(float(i + 268))+0.5))//floor函数作用是向下取整，如floor(1.5)=1
+		{
+			printf("%d\n", i);
+		}
+	}
+	return 0;
+}
+//题目：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
