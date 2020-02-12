5b95f1b7f3aa57c5d50ac979398ae14239240eca
guanchanglong <506921079@qq.com>
2020-02-12 20:19:23 +0800

    2_12

---
+++ C语言100道/52.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE 0
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char a[50];
+	int i = 0, j = 0;
+	printf("请输入整数(大于等于1000000):");
+	gets(a);
+	while (a[i] != '\0')
+	{
+		i++;
+	}
+	j = i - 7;
+	for (; j <= (i - 4); j++)
+	{
+		printf("%c", a[j]);
+	}
+	system("pause");
+	return 0;
+}
+//52.题目：取一个整数a从右端开始的4〜7位。
